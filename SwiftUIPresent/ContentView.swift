1f49e1cd85b25b1394916d0088dbe9a9a2a460f8
Samu Andras <samu.andris1@gmail.com>
2022-01-08 16:15:33 +0100

    Initial Commit

---
+++ SwiftUIPresent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPresent
+//
+//  Created by Samu András on 2022. 01. 08..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPresent/SwiftUIPresentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPresentApp.swift
+//  SwiftUIPresent
+//
+//  Created by Samu András on 2022. 01. 08..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPresentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
